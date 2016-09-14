30d674019983b8442c2e3123693021a7204e657c
Melita Kokot <melita.kokot@gmail.com>
2016-09-14 14:12:05 +0200

    Refactor action dsl and update specs

--- lib/dox/dsl/action.rb
+++ lib/dox/dsl/action.rb
@@ -9,28 +9,21 @@ module Dox
       attr_writer :desc
       attr_writer :params
 
-      def initialize(opts = {})
-        self.name = opts.fetch(:name, nil)
+      def initialize(name, &block)
+        self.name = name
+        instance_eval(&block) if block_given?
+
         raise(Dox::Errors::InvalidActionError, 'Action name is required!') if @name.blank?
-
-        self.verb = opts.fetch(:verb, nil)
-        self.path = opts.fetch(:path, nil)
-        self.desc = opts.fetch(:desc, nil)
-        self.params = opts.fetch(:params, nil)
-      end
-
-      def param(signature)
-        params << signature
       end
 
       def config
-        Hash.new.tap do |config|
-          config[:action_name] = @name.presence
-          config[:action_verb] = @verb.presence
-          config[:action_path] = @path.presence
-          config[:action_desc] = @desc.presence
-          config[:action_params] = @params.presence
-        end
+        {
+          action_name: @name.presence,
+          action_verb: @verb.presence,
+          action_path: @path.presence,
+          action_desc: @desc.presence,
+          action_params: @params.presence
+        }
       end
     end
   end
--- spec/dsl/action_spec.rb
+++ spec/dsl/action_spec.rb
@@ -1,21 +1,26 @@
 describe Dox::DSL::Action do
   subject { described_class }
 
+  ACTION_NAME = 'Get pokemons'
+  URI_PARAMS = { id: 1 }
+  VERB = 'GET'
+  PATH = '/pokemons'
+  DESC = 'Returns list of Pokemons'
+
   let(:options) do
-    {
-      name: 'Get pokemons',
-      verb: 'GET',
-      path: '/pokemons',
-      desc: 'Returns list of Pokemons',
-      params: { id: 1 }
-    }
+    Proc.new do
+      verb VERB
+      path PATH
+      desc DESC
+      params URI_PARAMS
+    end
   end
 
   describe '#initialize' do
     context 'when missing required attributes' do
       it 'raises error when name is not specified' do
         expect do
-          subject.new(options.except(:name))
+          subject.new(nil, &options)
         end.to raise_error(Dox::Errors::InvalidActionError, /name is required/)
       end
     end
@@ -23,18 +28,18 @@ describe Dox::DSL::Action do
     context 'when required attributes present' do
       it 'initializes action' do
         expect do
-          subject.new(options)
+          subject.new(ACTION_NAME, &options)
         end.not_to raise_error
       end
     end
   end
 
   describe '#config' do
-    let(:action) { subject.new(options) }
-    it { expect(action.config[:action_name]).to eq(options[:name]) }
-    it { expect(action.config[:action_verb]).to eq(options[:verb]) }
-    it { expect(action.config[:action_path]).to eq(options[:path]) }
-    it { expect(action.config[:action_desc]).to eq(options[:desc]) }
-    it { expect(action.config[:action_params]).to eq(options[:params]) }
+    let(:action) { subject.new(ACTION_NAME, &options) }
+    it { expect(action.config[:action_name]).to eq(ACTION_NAME) }
+    it { expect(action.config[:action_verb]).to eq(VERB) }
+    it { expect(action.config[:action_path]).to eq(PATH) }
+    it { expect(action.config[:action_desc]).to eq(DESC) }
+    it { expect(action.config[:action_params]).to eq(URI_PARAMS) }
   end
 end
