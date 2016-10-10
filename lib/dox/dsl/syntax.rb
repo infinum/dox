cedac9223cc2b74a74c8c1b05efb1d8777586aec
Melita Kokot <melita.kokot@gmail.com>
2016-10-10 21:32:33 +0200

    Refactor syntax module to avoid class_methods usage

--- lib/dox/dsl/syntax.rb
+++ lib/dox/dsl/syntax.rb
@@ -3,30 +3,28 @@ module Dox
     module Syntax
       extend ActiveSupport::Concern
 
-      class_methods do
-        def document(subject, &block)
-          documentation = _subjects[subject] = Documentation.new(subject: subject)
-          documentation.instance_eval(&block)
-        end
+      def document(subject, &block)
+        documentation = _subjects[subject] = Documentation.new(subject: subject)
+        documentation.instance_eval(&block)
+      end
 
-        def const_missing(name)
-          documentation = _subjects[infer_subject(name)]
-          return super unless documentation
+      def const_missing(name)
+        documentation = _subjects[infer_subject(name)]
+        return super unless documentation
 
-          Module.new do
-            define_singleton_method :included do |base|
-              base.metadata.merge! documentation.config
-            end
+        Module.new do
+          define_singleton_method :included do |base|
+            base.metadata.merge! documentation.config
           end
         end
+      end
 
-        def infer_subject(name)
-          name.to_s.underscore.to_sym
-        end
+      def infer_subject(name)
+        name.to_s.underscore.to_sym
+      end
 
-        def _subjects
-          @_subjects ||= {}
-        end
+      def _subjects
+        @_subjects ||= {}
       end
     end
   end
