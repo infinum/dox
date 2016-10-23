9786d510bbaa1bc13ba91bdcb55115d97dae0de0
Melita Kokot <melita.kokot@gmail.com>
2016-10-23 14:03:38 +0200

    Fix some codeclimate issues

--- lib/dox/config.rb
+++ lib/dox/config.rb
@@ -4,12 +4,12 @@ module Dox
     attr_reader :header_file_path, :desc_folder_path
 
     def header_file_path=(file_path)
-      raise(Errors::FileNotFoundError, file_path) unless File.exists?(file_path)
+      raise(Errors::FileNotFoundError, file_path) unless File.exist?(file_path)
       @header_file_path = file_path
     end
 
     def desc_folder_path=(folder_path)
-      raise(Errors::FolderNotFoundError, folder_path) unless Dir.exists?(folder_path)
+      raise(Errors::FolderNotFoundError, folder_path) unless Dir.exist?(folder_path)
       @desc_folder_path = folder_path
     end
 
