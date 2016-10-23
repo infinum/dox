9df31326adf79af340384cd0d08a9110832cf694
Melita Kokot <melita.kokot@gmail.com>
2016-10-23 12:58:51 +0200

    Guess path params from request

--- lib/dox/entities/action.rb
+++ lib/dox/entities/action.rb
@@ -10,7 +10,7 @@ module Dox
         @desc = details[:action_desc]
         @verb = details[:action_verb] || request.method
         @path = details[:action_path] || template_path
-        @uri_params = details[:action_params]
+        @uri_params = details[:action_params] || template_path_params
         @examples = []
       end
 
@@ -18,14 +18,34 @@ module Dox
 
       attr_reader :request
 
+      # /pokemons/1 => pokemons/{id}
       def template_path
-        path_params = request.path_parameters.symbolize_keys.except(:action, :controller)
         path = request.path.dup
         path_params.each do |key, value|
-          # /pokemons/1 => pokemons/{id}
-          path.sub!(/\/#{value}(\/|$)/, "/{#{key}}\\1")
+          path.sub!(%r{\/#{value}(\/|$)}, "/{#{key}}\\1")
         end
         path
+      end
+
+      def path_params
+        @path_params ||= request.path_parameters.symbolize_keys.except(:action, :controller, :format)
+      end
+
+      def template_path_params
+        h = {}
+        path_params.each do |param, value|
+          param_type = guess_param_type(value)
+          h[param] = { type: param_type, required: :required, value: value }
+        end
+        h
+      end
+
+      def guess_param_type(param)
+        if param =~ /^\d+$/
+          :number
+        else
+          :string
+        end
       end
     end
   end
