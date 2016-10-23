aed1e49431d9641a8fc26f71333bc2b1cd0ca4b4
Melita Kokot <melita.kokot@gmail.com>
2016-10-23 14:09:53 +0200

    Style changes

--- lib/dox/config.rb
+++ lib/dox/config.rb
@@ -1,6 +1,5 @@
 module Dox
   class Config
-
     attr_reader :header_file_path, :desc_folder_path
 
     def header_file_path=(file_path)
@@ -12,7 +11,5 @@ module Dox
       raise(Errors::FolderNotFoundError, folder_path) unless Dir.exist?(folder_path)
       @desc_folder_path = folder_path
     end
-
   end
 end
-
