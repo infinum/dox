968f8503eb3b7e5ee7ea9b897326db8fad905d63
Melita Kokot <melita.kokot@gmail.com>
2016-10-24 17:21:19 +0200

    Move codeclimate reporter to the top of spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 require 'active_support'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/object/json'
 require 'active_support/core_ext/string'
 require 'active_support/hash_with_indifferent_access'
-require 'codeclimate-test-reporter'
 require 'dox'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-CodeClimate::TestReporter.start
