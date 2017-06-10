30df179c2499eb562d9efa75c4b34d08a8b72f09
Melita Kokot <melita.kokot@gmail.com>
2017-06-10 18:26:24 +0200

    Update example entity tests

--- spec/entities/example_spec.rb
+++ spec/entities/example_spec.rb
@@ -6,7 +6,7 @@ describe Dox::Entities::Example do
   let(:example_desc) { 'Dummy' }
   let(:query_params) { { 'color' => 'blue'} }
   let(:path_params) { { 'id' => 11 } }
-  let(:body_parameters) { { 'data' => 'users' } }
+  let(:body) { { 'data' => 'users' } }
   let(:request_fullpath) { '/pokemons?color=blue' }
 
   let(:response) { double('response', content_type: content_type, status: 200, body: response_body) }
@@ -14,21 +14,17 @@ describe Dox::Entities::Example do
 
   let(:example) { subject.new({ description: example_desc }, request, response) }
 
-  describe '#request_parameters' do
-    context 'with query and path parameters only' do
-      before { allow(request).to receive(:parameters).and_return(query_params.merge(path_params)) }
+  describe '#request_body' do
+    context 'when empty' do
+      before { allow(request).to receive(:body).and_return(StringIO.new) }
 
-      it 'returns empty request parameters' do
-        expect(example.request_parameters).to eq({})
-      end
+      it { expect(example.request_body).to eq('') }
     end
 
-    context 'with body paramaters too' do
-      before { allow(request).to receive(:parameters).and_return(query_params.merge(path_params).merge(body_parameters)) }
+    context 'when present' do
+      before { allow(request).to receive(:body).and_return(StringIO.new(body.to_json)) }
 
-      it 'returns request body parameters' do
-        expect(example.request_parameters).to eq(body_parameters)
-      end
+      it { expect(example.request_body).to eq(body) }
     end
   end
 
