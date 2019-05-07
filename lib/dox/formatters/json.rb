05a68c11fe0f6380a6f27cd10afd8d3f34071579
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2019-05-07 09:50:46 +0200

    Remove rescuing JSON parsing error in JSON formatter

--- lib/dox/formatters/json.rb
+++ lib/dox/formatters/json.rb
@@ -2,9 +2,12 @@ module Dox
   module Formatters
     class Json < Dox::Formatters::Base
       def format
-        JSON.pretty_generate(JSON.parse(body || ''))
-      rescue JSON::ParserError
-        ''
+        # in cases where the body isn't valid JSON
+        # and the headers specify the Content-Type is application/json
+        # an error should be raised
+        return '' if body.nil? || body.length < 2
+
+        JSON.pretty_generate(JSON.parse(body))
       end
     end
   end
